0f65ed90edad59e60b94196fc278c6a4395dee06
MattDyer87 <71666354+MattDyer87@users.noreply.github.com>
2020-11-16 16:34:07 -0800

    add sketch3

---
+++ processingprograms/sketch3/sketch.js
@@ -0,0 +1,23 @@
+var x = 0;
+var y = 0;
+var px = 0;
+var py = 0;
+var easing = 0.05;
+
+function setup() {
+    createCanvas(1400,475);
+    stroke(102);
+}
+function draw() {
+    if (mouseIsPressed) {
+        var targetX = mouseX;
+        x += (targetX - x) * easing;
+        var targetY = mouseY;
+        y += (targetY - y) * easing;
+        var weight = dist(x, y, px, py);
+        line(x, y, px, py);
+        strokeWeight(weight);
+        py = y;
+        px = x;
+    }
+}
